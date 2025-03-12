c2758b0a46961c793f6575fed86609d9e6e4fcf8
vaibhav8772 <manev4653@gmail.com>
2025-03-12 15:26:34 +0530

    first commit

---
+++ src/main/java/com/tca/ASS01HBFirstProject/App.java
@@ -0,0 +1,68 @@
+package com.tca.ASS01HBFirstProject;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.tca.entities.Student;
+
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        Configuration configuration = null;
+        SessionFactory sessionfactory =null;
+        Session session=null;
+        Transaction transaction=null;
+        
+        try {
+        	
+        	
+        	 configuration = new Configuration();
+        	
+        	configuration.configure("hibernate.cfg.xml");
+        	
+        	
+        	
+        	
+            sessionfactory = configuration.buildSessionFactory();
+            session = sessionfactory.openSession();
+            transaction =session.beginTransaction();
+        	
+            
+            Student student = new Student();
+            student.setRno(101);
+            student.setName("AAA");
+            student.setPer(70.80);
+            
+            session.save(student);
+            
+               
+            transaction.commit();
+            System.out.println("Data Saved Successfully");
+       	
+        }
+        
+        
+        
+        catch(Exception e) {
+        	e.printStackTrace();
+        	transaction.rollback();
+        	
+        }
+        
+        finally {
+        	
+        	
+        	
+        	
+        }
+        
+     
+    }
+}
